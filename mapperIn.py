809ee7c77509ea27efe6615e2794e528fa585d7d
Naveen Tanuku <ntanuku96@gmail.com>
2022-01-28 12:05:45 -0600

    Create mapperIn.py

---
+++ mapperIn.py
@@ -0,0 +1,18 @@
+# Author: Naveen Tanuku
+# Created: 28 Jan 2022
+# Updated: 28 Jan 2022
+# A simple map reduce program
+
+f = open("purchases.txt","r")  # open file, read-only
+o = open("outputData.txt", "w") # open file, write
+for line in f:  
+    rowList = line.strip().split("    ") # DT: List of Lists
+    print (rowList )
+    print (len(rowList ))
+    if len(rowList) == 6:
+        date, time, location, dept, amount, payType = rowList
+        #assign names
+        print ("{0}\t{1}".format(location, amount))
+        o.write("{0}\t{1}\n".format(location, amount))
+f.close()
+o.close()
